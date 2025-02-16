b48d250e3590c8256a5772e170d0c6027ab5175e
Manidhar Karnatakam <manidhar.karnatakam@gmail.com>
2025-02-16 09:00:07 +0530

    connecting to mongodb and push the data into it

--- stud-per.py
+++ stud-per.py
@@ -3,7 +3,14 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler,LabelEncoder
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+uri = "mongodb+srv://samuthasya:samuthasya@cluster0.g4evw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db=client['student']
+collection=db['student_perd']
 
 def load_model():
     with open('student_lr_final_model.pkl', 'rb') as file:
@@ -42,6 +49,9 @@ def main():
             }
         prediction = predict_data(user_data)
         st.success(f"Your Predicted Score is {prediction}")
+        user_data['Predicted Score'] = round(float(prediction[0]),2)
+        user_data = {key: int(value) if isinstance(value, np.integer) else float(value) if isinstance(value, np.floating) else value for key, value in user_data.items()}
+        collection.insert_one(user_data)
 
 
 if __name__ == '__main__':
